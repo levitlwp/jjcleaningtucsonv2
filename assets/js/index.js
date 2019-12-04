17c272d11b509ab7e8c098789dad6ad63e8243ef
levitlwp <levitlwp@gmail.com>
2019-12-03 20:31:07 -0700

    Add the change content function of intro section and fixed padding of icons section

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,15 @@
 const introItems = document.querySelectorAll('.introduction__item');
 const introItemsArr = Array.from(introItems);
+
 const introLinks = document.querySelectorAll('.introduction__link');
 const introLinksArr = Array.from(introLinks);
+
+const introDetailHeading = document.querySelectorAll(
+  '.introduction__detail-heading'
+);
+const introDetailHeadingArr = Array.from(introDetailHeading);
+
+const introHeading = document.querySelector('.introduction__heading');
 
 const clearIntroActive = () => {
   introItemsArr.forEach(el => {
@@ -17,9 +25,75 @@ introItemsArr.forEach((el, index) => {
     e.preventDefault();
     // 1. Clear all active state
     clearIntroActive();
-    // 2. Change index 0 to dark text and change current text color to white
+    // 2. Change current event to active
     introLinksArr[index].classList.add('introduction__link--active');
     el.classList.toggle('introduction__item--active');
+    // 3. Change the content
+
+    // RESIDENTIAL
+    if (index === 0) {
+      // Details
+      introDetailHeadingArr.forEach((el, index) => {
+        if (index === 0) {
+          el.textContent = 'Furniture treatment';
+        } else if (index == 1) {
+          el.textContent = 'Packing and unpacking';
+        } else if (index === 2) {
+          el.textContent = 'Window washing';
+        }
+      });
+
+      // Heading on the right
+      introHeading.textContent = 'Superior result';
+    }
+
+    // BUSINESS
+    if (index === 1) {
+      introDetailHeadingArr.forEach((el, index) => {
+        if (index === 0) {
+          el.textContent = 'Janitorial services';
+        } else if (index == 1) {
+          el.textContent = 'Speciality services';
+        } else if (index === 2) {
+          el.textContent = 'Flooring';
+        }
+      });
+
+      // Heading on the right
+      introHeading.textContent = 'Superior results';
+    }
+
+    // EXTERIOR
+    if (index === 2) {
+      introDetailHeadingArr.forEach((el, index) => {
+        if (index === 0) {
+          el.textContent = 'Window cleaning';
+        } else if (index == 1) {
+          el.textContent = 'Gutter cleaning';
+        } else if (index === 2) {
+          el.textContent = 'Driveway resealing';
+        }
+      });
+
+      // Heading on the right
+      introHeading.textContent = 'Super external clean';
+    }
+
+    // GREEN
+    if (index === 3) {
+      introDetailHeadingArr.forEach((el, index) => {
+        if (index === 0) {
+          el.textContent = 'Kitchens';
+        } else if (index == 1) {
+          el.textContent = 'Bathrooms';
+        } else if (index === 2) {
+          el.textContent = 'Seasonal';
+        }
+      });
+
+      // Heading on the right
+      introHeading.textContent = 'Get started now!';
+    }
   });
 });
 
