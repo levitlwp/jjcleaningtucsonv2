9958146a13d226f0007b281687bff42d04a7389e
levitlwp <levitlwp@gmail.com>
2019-12-17 21:45:17 -0700

    Redesigned about-us page. Fixed issues with CountUp on about-us page. Fixed issues with introduction section on home page

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,7 @@
-/**
- * HEADER CAROUSEL
- */
-$(document).ready(function() {
-  $(".owl-carousel").owlCarousel();
-});
+/* Header carousel */
+const owl1 = $('.owl-carousel--1');
 
-$(".owl-carousel--1").owlCarousel({
+$('.owl-carousel--1').owlCarousel({
   loop: true,
   margin: 0,
   nav: false,
@@ -25,38 +21,52 @@ $(".owl-carousel--1").owlCarousel({
   }
 });
 
-$(".owl-carousel--2").owlCarousel({
+// Go to the previous item
+Array.from($('.header__icon--left')).forEach(el => {
+  el.closest('.header__icon--left').addEventListener('click', e => {
+    owl1.trigger('prev.owl.carousel', [500]);
+  });
+});
+
+// Go to the next item
+Array.from($('.header__icon--right')).forEach(el => {
+  el.closest('.header__icon--right').addEventListener('click', e => {
+    owl1.trigger('next.owl.carousel', [500]);
+  });
+});
+
+$('.owl-carousel--2').owlCarousel({
   loop: true,
   margin: 40,
-  nav: false,
+  nav: true,
   dots: false,
   responsive: {
     0: {
       items: 1
     },
     600: {
-      items: 2
+      items: 1
     },
     1000: {
-      items: 3
+      items: 1
     }
   }
 });
 
-const owl = $(".owl-carousel--1");
-owl.owlCarousel();
+const owl2 = $('.owl-carousel--2');
+owl2.owlCarousel();
 
 // Go to the previous item
-Array.from($(".header__icon--left")).forEach(el => {
-  el.closest(".header__icon--left").addEventListener("click", e => {
-    owl.trigger("prev.owl.carousel", [500]);
+Array.from($('.clients__icon--left')).forEach(el => {
+  el.closest('.clients__icon--left').addEventListener('click', e => {
+    owl2.trigger('prev.owl.carousel', [500]);
   });
 });
 
 // Go to the next item
-Array.from($(".header__icon--right")).forEach(el => {
-  el.closest(".header__icon--right").addEventListener("click", e => {
-    owl.trigger("next.owl.carousel", [500]);
+Array.from($('.header__icon--right')).forEach(el => {
+  el.closest('.header__icon--right').addEventListener('click', e => {
+    owl2.trigger('next.owl.carousel', [500]);
   });
 });
 
@@ -79,56 +89,65 @@ Array.from($(".header__icon--right")).forEach(el => {
 //   }
 // });
 
-const headerNavItems = document.querySelectorAll(".headernav__item");
+/* Navbar Scripts */
+// jQuery to collapse the navbar on scroll
+$(window).on('scroll load', function() {
+  if ($('.navigation').offset().top > 20) {
+    $('.navigation').addClass('navigation--collapse');
+  } else {
+    $('.navigation').removeClass('navigation--collapse');
+  }
+});
+
+/* Headernav & introduction Scripts */
+const headerNavItems = document.querySelectorAll('.headernav__item');
 const headerNavItemsArr = Array.from(headerNavItems);
 
-const headerNavLinks = document.querySelectorAll(".headernav__link");
+const headerNavLinks = document.querySelectorAll('.headernav__link');
 const headerNavLinksArr = Array.from(headerNavLinks);
 
 const introDetailHeading = document.querySelectorAll(
-  ".introduction__detail-heading"
+  '.introduction__detail-heading'
 );
-
 const introDetailHeadingArr = Array.from(introDetailHeading);
 
-const introIcons = document.querySelectorAll(".introduction__icon");
+const introIcons = document.querySelectorAll('.introduction__icon');
 const introIcsonsArr = Array.from(introIcons);
 
 const introDetailBottom = document.querySelectorAll(
-  ".introduction__detail-bottom"
+  '.introduction__detail-bottom'
 );
 const introDetailBottomArr = Array.from(introDetailBottom);
 
-const introHeading = document.querySelector(".introduction__heading");
+const introHeading = document.querySelector('.introduction__heading');
 
 // Clear introduction active state
 const clearIntroActive = () => {
   headerNavItems.forEach(el => {
-    el.classList.remove("headernav__item--active");
+    el.classList.remove('headernav__item--active');
   });
   headerNavLinksArr.forEach(el => {
-    el.classList.remove("headernav__link--active");
+    el.classList.remove('headernav__link--active');
   });
 };
 
 // Collapse all contents
 const collapseDetailContent = () => {
   introDetailBottomArr.forEach(el => {
-    el.classList.remove("introduction__detail-bottom--toggle");
-    el.classList.add("introduction__detail-bottom--toggle");
+    $(el).hide(300);
   });
 };
 
 // Default all icons
 const defaultIcons = () => {
   introIcsonsArr.forEach(el => {
-    el.classList.remove("fa-minus");
-    el.classList.add("fa-plus");
+    el.classList.remove('fa-minus');
+    el.classList.add('fa-plus');
   });
 };
 
 headerNavItems.forEach((el, index) => {
-  el.addEventListener("click", e => {
+  el.closest('.headernav__item').addEventListener('click', e => {
     e.preventDefault();
 
     // 1. Clear all active state
@@ -139,8 +158,8 @@ headerNavItems.forEach((el, index) => {
     defaultIcons();
 
     // 3. Change current event to active
-    headerNavLinksArr[index].classList.add("headernav__link--active");
-    el.classList.toggle("headernav__item--active");
+    headerNavLinksArr[index].classList.add('headernav__link--active');
+    el.classList.toggle('headernav__item--active');
 
     // 4. Change the content
     // RESIDENTIAL
@@ -148,72 +167,72 @@ headerNavItems.forEach((el, index) => {
       // Details
       introDetailHeadingArr.forEach((el, index) => {
         if (index === 0) {
-          el.textContent = "Furniture treatment";
+          el.textContent = 'Furniture treatment';
         } else if (index == 1) {
-          el.textContent = "Packing and unpacking";
+          el.textContent = 'Packing and unpacking';
         } else if (index === 2) {
-          el.textContent = "Window washing";
+          el.textContent = 'Window washing';
         }
       });
 
       // Heading on the right
-      introHeading.textContent = "Need home cleaning?";
+      introHeading.textContent = 'Need home cleaning?';
     }
 
     // BUSINESS
     if (index === 1) {
       introDetailHeadingArr.forEach((el, index) => {
         if (index === 0) {
-          el.textContent = "Janitorial services";
+          el.textContent = 'Janitorial services';
         } else if (index == 1) {
-          el.textContent = "Speciality services";
+          el.textContent = 'Speciality services';
         } else if (index === 2) {
-          el.textContent = "Flooring";
+          el.textContent = 'Flooring';
         }
       });
 
       // Heading on the right
-      introHeading.textContent = "Superior results";
+      introHeading.textContent = 'Superior results';
     }
 
     // EXTERIOR
     if (index === 2) {
       introDetailHeadingArr.forEach((el, index) => {
         if (index === 0) {
-          el.textContent = "Window cleaning";
+          el.textContent = 'Window cleaning';
         } else if (index == 1) {
-          el.textContent = "Gutter cleaning";
+          el.textContent = 'Gutter cleaning';
         } else if (index === 2) {
-          el.textContent = "Driveway resealing";
+          el.textContent = 'Driveway resealing';
         }
       });
 
       // Heading on the right
-      introHeading.textContent = "Super external clean";
+      introHeading.textContent = 'Super external clean';
     }
 
     // GREEN
     if (index === 3) {
       introDetailHeadingArr.forEach((el, index) => {
         if (index === 0) {
-          el.textContent = "Kitchens";
+          el.textContent = 'Kitchens';
         } else if (index == 1) {
-          el.textContent = "Bathrooms";
+          el.textContent = 'Bathrooms';
         } else if (index === 2) {
-          el.textContent = "Seasonal";
+          el.textContent = 'Seasonal';
         }
       });
 
       // Heading on the right
-      introHeading.textContent = "Get started now!";
+      introHeading.textContent = 'Get started now!';
     }
   });
 });
 
 introIcsonsArr.forEach((el, index) => {
-  el.closest(".introduction__detail-top").addEventListener("click", e => {
-    el.classList.toggle("fa-plus");
-    el.classList.toggle("fa-minus");
+  el.closest('.introduction__detail-top').addEventListener('click', e => {
+    el.classList.toggle('fa-plus');
+    el.classList.toggle('fa-minus');
     $(introDetailBottom[index]).slideToggle(300);
   });
 });
@@ -221,14 +240,14 @@ introIcsonsArr.forEach((el, index) => {
 /**
  * SERVICES PAGES QUESTION
  */
-const servicesQuestions = $(".services__question");
+const servicesQuestions = $('.services__question');
 const servicesQuestionsArr = Array.from(servicesQuestions);
 
-const servicesAnswers = $(".services__question--bottom");
+const servicesAnswers = $('.services__question--bottom');
 const servicesAnswersArr = Array.from(servicesAnswers);
 
 servicesQuestionsArr.forEach((el, index) => {
-  el.addEventListener("click", e => {
+  el.addEventListener('click', e => {
     $(servicesAnswersArr[index]).slideToggle(300);
   });
 });
@@ -236,24 +255,34 @@ servicesQuestionsArr.forEach((el, index) => {
 /**
  *  About-us CountUp number animation
  */
-$(".counter").each(function() {
-  var $this = $(this),
-    countTo = $this.attr("data-count");
+let a = 0;
+$(window).scroll(function() {
+  if ($('.numbers').length) {
+    const oTop = $('.numbers').offset().top - window.innerHeight;
 
-  $({ countNum: $this.text() }).animate(
-    {
-      countNum: countTo
-    },
+    if (a === 0 && $(window).scrollTop() > oTop) {
+      $('.counter').each(function() {
+        var $this = $(this),
+          countTo = $this.attr('data-count');
 
-    {
-      duration: 2000,
-      easing: "linear",
-      step: function() {
-        $this.text(Math.floor(this.countNum));
-      },
-      complete: function() {
-        $this.text(this.countNum);
-      }
+        $({ countNum: $this.text() }).animate(
+          {
+            countNum: countTo
+          },
+
+          {
+            duration: 2000,
+            easing: 'swing',
+            step: function() {
+              $this.text(Math.floor(this.countNum));
+            },
+            complete: function() {
+              $this.text(this.countNum);
+            }
+          }
+        );
+      });
+      a = 1;
     }
-  );
+  }
 });
